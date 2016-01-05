c443f5286f5ecd59dbbf1077b0ec986cacfd8612
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-05 16:12:23 -0500

    changed the ES data loader to accept IP and port as params

--- code/load_data.py
+++ code/load_data.py
@@ -1,13 +1,23 @@
 import requests
+import sys
 from elasticsearch import Elasticsearch
+from optparse import OptionParser
 
-es = Elasticsearch()
+
+usage = "%prog [options]\n\
+       eg. python %prog -p 9200 -i 127.0.0.1"
+
+parser = OptionParser(usage)
+parser.add_option("-p", "--port", action="store",
+                  help="port number of the ES service")
+parser.add_option("-i", "--ip", action="store",
+                  help="ip / host name of the ES service")
 
 def getData(url):
     r = requests.get(url)
     return r.json()
 
-def loadToES():
+def loadToES(es):
     url = "http://data.sfgov.org/resource/rqzj-sfat.json"
     data = getData(url)
     print "Loading data ..."
@@ -16,4 +26,9 @@ def loadToES():
     print "Total trucks loaded: ", len(data)
 
 if __name__ == "__main__":
-    loadToES()
+    (options, args) = parser.parse_args()
+    if not options.ip or not options.port:
+        print "Please provide port and ip. see -h"
+        sys.exit()
+    es = Elasticsearch(options.ip, port=options.port)
+    loadToES(es)
