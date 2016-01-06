60f523776cfeec4265ecfdc47ead9f213e51ea33
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-05 21:52:30 -0500

    Added a debug route in ES app. Created dockerfile for flask app

---
+++ code/Dockerfile
@@ -0,0 +1,8 @@
+# our base image
+from python:3-onbuild
+
+# expose the port
+EXPOSE 5000
+
+# run the command
+CMD [ "python", "./app.py" ]
--- code/app.py
+++ code/app.py
@@ -1,12 +1,27 @@
 from elasticsearch import Elasticsearch
 from flask import Flask, jsonify, request
+from urllib3 import exceptions
 
+from elasticsearch import Elasticsearch
 es = Elasticsearch()
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "hello world"
+
+@app.route('/debug')
+def test_es():
+    resp = {}
+    try:
+        msg = es.cat.indices()
+        resp["msg"] = msg
+        resp["status"] = "success"
+    except:
+        resp["status"] = "failure"
+        resp["msg"] = "Unable to reach ES"
+    return jsonify(resp)
 
 @app.route('/search')
 def search():
@@ -19,4 +34,4 @@ def search():
     return jsonify(res["hits"])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
