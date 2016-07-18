2c315535eb695f23765f43d9c918db0739bde3bc
Prakhar Srivastav <prakhar1989@gmail.com>
2016-07-18 17:52:09 -0400

    add google analytics snippet

--- build/tweaks/analytics.py
+++ build/tweaks/analytics.py
@@ -1,16 +1,11 @@
 script = '''
-<!-- Asynchronous Google Analytics snippet -->
 <script>
-    var _gaq = _gaq || [];
-    var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-    _gaq.push(['_setAccount', 'UA-18504951-1']);
-    _gaq.push(['_trackPageview']);
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+  ga('create', 'UA-40692332-3', 'auto');
+  ga('send', 'pageview');
 </script>
 '''
