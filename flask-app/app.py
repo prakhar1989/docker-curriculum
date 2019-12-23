e4f20537d32bcde97c01ec4e66a624d7d33e4856
Prakhar Srivastav <prsrivastav@google.com>
2019-12-23 20:50:04 +0530

    use environment variable for port

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 import random
 
 app = Flask(__name__)
@@ -18,10 +19,12 @@ images = [
     "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F9.gif?alt=media&token=a57a1c71-a8af-4170-8fee-bfe11809f0b3",
 ]
 
-@app.route('/')
+
+@app.route("/")
 def index():
     url = random.choice(images)
-    return render_template('index.html', url=url)
+    return render_template("index.html", url=url)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
